2badf061a89f221f72a676cf79dc58d378114e91
Martin <martinroige@hotmail.com>
2023-08-22 08:18:14 -0300

    Todos los ejercicios resueltos

--- integradores.py
+++ integradores.py
@@ -99,7 +99,7 @@ class Persona:
     """
     Clase Persona con atributos de Nombre, Edad y DNI. Métodos Getters y Setters y un mostrar 
     """
-    def __init__(self, nuevo_nombre = "", nueva_edad = 0, nuevo_dni = 0):
+    def __init__(self, nuevo_nombre = "", nueva_edad = 0, nuevo_DNI = 0):
         self._nombre = nuevo_nombre
         try:
             self._edad = int(nueva_edad)
@@ -109,7 +109,14 @@ class Persona:
         except:
             self._edad = 0
             print("Edad inválida, deberá volver a setearla luego")
-        self._DNI = nuevo_dni
+        try:
+            self._DNI = int(nuevo_DNI)
+            if ((nuevo_DNI<100000) or (nuevo_DNI>100000000)):
+                print("DNI inválido, deberá volver a setearlo luego")
+                self._DNI = 0
+        except:
+            self._DNI = 0
+            print("DNI inválido, deberá volver a setearlo luego")
     @property
     def nombre(self):
         return self._nombre
@@ -135,11 +142,14 @@ class Persona:
             print("Edad inválida, deberá volver a setearla luego")
     @DNI.setter
     def DNI(self, nuevo_DNI):
-        nuevo_DNI_int=get_int(nuevo_DNI)
-        if (nuevo_DNI_int>1000000) and (nuevo_DNI_int<100000000):
-            self._DNI=nuevo_DNI_int
-        else:
-            raise ValueError("El DNI debe estar comprendido entre 1.000.000 y 100.000.000")
+        try:
+            self._DNI = int(nuevo_DNI)
+            if ((nuevo_DNI<100000) or (nuevo_DNI>100000000)):
+                print("DNI inválido, deberá volver a setearlo luego")
+                self._DNI = 0
+        except:
+            self._DNI = 0
+            print("DNI inválido, deberá volver a setearlo luego")
         
     def mostrar(self):
         print(f"Nombre: {self._nombre}, Edad: {self._edad}, DNI: {self._DNI}")
@@ -193,10 +203,32 @@ class CuentaJoven(Cuenta):
             super().retirar(monto)
 
     def mostrar(self):
-        print("Cuenta Joven")
+        print("* Cuenta Joven *")
         super().mostrar()
         print(f"Bonificación: {self._bonificacion}")
 
-empleado1 = CuentaJoven("Pepe",25,35678910,1500,25)
-empleado1.mostrar()
+persona1 = Persona("Juan",36,31546879)
+persona1.mostrar()
+persona1.nombre = "Juan Perez"
+persona1.edad = 39
+persona1.DNI = 41202303
+persona1.mostrar()
+print(f"Nombre: {persona1.nombre}, edad: {persona1.edad}, DNI: {persona1.DNI}")
+
+cuenta1 = Cuenta("Maria",45,21345678,3500)
+cuenta1.mostrar()
+cuenta1.ingresar(2000)
+cuenta1.mostrar()
+cuenta1.retirar(8000)
+cuenta1.mostrar()
+
+cuentajoven1 = CuentaJoven("Pepe",25,35264548,1500,25)
+cuentajoven1.mostrar()
+print(cuentajoven1.es_titular_valido())
+cuentajoven1.retirar(100)
+cuentajoven1.mostrar()
+cuentajoven1.edad = 24
+print(cuentajoven1.es_titular_valido())
+cuentajoven1.retirar(100)
+cuentajoven1.mostrar()
 
